e2457dd995a713fd510595bed948d25eac395c7e
Kamrul Ahmed <kamrul_sscis@hotmail.com>
2018-07-27 14:18:53 +0100

    Laravel nuxt

---
+++ resources/nuxt/layouts/default.vue
@@ -0,0 +1,64 @@
+<template>
+    <div id="app">
+        <v-app id="inspire" dark>
+            <v-navigation-drawer
+                clipped
+                fixed
+                v-model="drawer"
+                app
+            >
+                <v-list dense>
+                    <v-list-tile @click="">
+                        <v-list-tile-action>
+                            <v-icon>dashboard</v-icon>
+                        </v-list-tile-action>
+                        <v-list-tile-content>
+                            <v-list-tile-title @click="onNavigate('/')">
+                                Home
+                            </v-list-tile-title>
+                        </v-list-tile-content>
+                    </v-list-tile>
+                    <v-list-tile @click="">
+                        <v-list-tile-action>
+                            <v-icon>settings</v-icon>
+                        </v-list-tile-action>
+                        <v-list-tile-content @click="onNavigate('/about')">
+                            <v-list-tile-title>About us</v-list-tile-title>
+                        </v-list-tile-content>
+                    </v-list-tile>
+                </v-list>
+            </v-navigation-drawer>
+            <v-toolbar app fixed clipped-left>
+                <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+                <v-toolbar-title>Application</v-toolbar-title>
+            </v-toolbar>
+            <v-content>
+                <nuxt/>
+            </v-content>
+            <v-footer app fixed>
+                <span>&copy; 2018</span>
+            </v-footer>
+        </v-app>
+    </div>
+</template>
+<script>
+    export default {
+        data: () => ({
+            drawer: true
+        }),
+
+        props: {
+            source: String
+        },
+
+        methods:{
+            onClick(){
+                this.$store.commit('increment');
+            },
+
+            onNavigate(link){
+                this.$router.push({path: link});
+            }
+        }
+    }
+</script>
---
+++ resources/nuxt/pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <v-container fluid fill-height>
+            <v-layout justify-center align-center>
+                <v-flex shrink class="text-xs-center">
+                    <h1>Hello {{ name }}!</h1>
+                    <v-btn dark raised @click="$store.commit('increment')">Click me {{ $store.getters.getCounter}}</v-btn>
+                </v-flex>
+            </v-layout>
+        </v-container>
+    </div>
+</template>
+
+<script>
+    export default {
+        asyncData (context) {
+            // called every time before loading the component
+            return {
+                name: 'Async data'
+            }
+        },
+        fetch () {
+            // The `fetch` method is used to fill the store before rendering the page
+        },
+        head () {
+            return {
+                title: 'new title',
+                meta: [
+                    { hid: 'description', name: 'description', content: 'My custom description' }
+                ]
+            }
+        },
+        data: () => {
+            return {
+
+            };
+        },
+    };
+</script>
