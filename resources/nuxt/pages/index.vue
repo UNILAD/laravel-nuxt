f8564d9afa2a27961b01e4eea63029b6df373c09
Kamrul Ahmed <kamrul_sscis@hotmail.com>
2018-07-27 16:57:07 +0100

    Implementing ssr

--- resources/nuxt/pages/index.vue
+++ resources/nuxt/pages/index.vue
@@ -4,7 +4,17 @@
             <v-layout justify-center align-center>
                 <v-flex shrink class="text-xs-center">
                     <h1>Hello {{ name }}!</h1>
-                    <v-btn dark raised @click="$store.commit('increment')">Click me {{ $store.getters.getCounter}}</v-btn>
+                    <v-btn dark raised @click="$store.commit('increment')">Click me {{ $store.getters.getCounter}}
+                    </v-btn>
+
+                    <v-autocomplete
+                    :items="countries"
+                    v-model="country"
+                    return-object
+                    @input="onInput()"
+                    >
+
+                    </v-autocomplete>
                 </v-flex>
             </v-layout>
         </v-container>
@@ -12,28 +22,66 @@
 </template>
 
 <script>
+
     export default {
-        asyncData (context) {
+        asyncData(context) {
             // called every time before loading the component
             return {
-                name: 'Async data'
+                name: 'Async data',
             }
         },
-        fetch () {
+        fetch() {
             // The `fetch` method is used to fill the store before rendering the page
         },
-        head () {
+        head() {
             return {
                 title: 'new title',
                 meta: [
-                    { hid: 'description', name: 'description', content: 'My custom description' }
+                    {hid: 'description', name: 'description', content: 'My custom description'}
                 ]
             }
         },
         data: () => {
             return {
-
+                search: '',
+                country: null,
+                countries: [
+                    {
+                        value: '1',
+                        text: 'one'
+                    },
+                    {
+                        value: '2',
+                        text: 'two'
+                    },
+                    {
+                        value: '3',
+                        text: 'three'
+                    },
+                    {
+                        value: '4',
+                        text: 'Four'
+                    },
+                    {
+                        value: '5',
+                        text: 'Five'
+                    },
+                    {
+                        value: '6',
+                        text: 'Six'
+                    }
+                ],
             };
         },
+
+        methods: {
+            onSearch() {
+                console.log('dlk');
+            },
+
+            onInput(){
+                console.log(this.country);
+            }
+        }
     };
 </script>
