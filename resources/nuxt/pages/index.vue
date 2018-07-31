d0d65686208e1a85b8462257a219580173642c6f
Kamrul Ahmed <kamrul_sscis@hotmail.com>
2018-07-31 09:53:34 +0100

    Index page update

--- resources/nuxt/pages/index.vue
+++ resources/nuxt/pages/index.vue
@@ -4,6 +4,7 @@
             <v-layout justify-center align-center>
                 <v-flex shrink class="text-xs-center">
                     <h1>Hello {{ name }}!</h1>
+                    <p>All dependencies install from sniffr</p>
                     <v-btn dark raised @click="$store.commit('increment')">Click me {{ $store.getters.getCounter}}
                     </v-btn>
 
